4bab9bd569c5988308f80408b0c5e0c29870156a
Jordan~ <jordan.brewer509@gmail.com>
2023-02-28 16:44:33 -0800

    delete movie functionality is coded

---
+++ src/reducers/favoriteReducer.js
@@ -0,0 +1,15 @@
+
+
+const initialState = {
+    favorites: [],
+    displayFavorites: true
+}
+
+const favoriteReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return(state);
+    }
+}
+
+export default favoriteReducer;
